ec43b85737471f19edc7e81fb77d94e29b87b7c1
olivier.solliec <olivier.solliec@d04796a0-1e52-11df-b145-f921259a7feb>
2010-03-06 10:54:47 +0000

    Création du code du serveur et du thread principal, en attente du client pour faire les premiers tests

--- server/ThreadPrincipal.cpp
+++ server/ThreadPrincipal.cpp
@@ -1,1 +1,20 @@
 #include "ThreadPrincipal.h"
+
+ThreadPrincipal::ThreadPrincipal(int _numeroClient, sf::IPAddress _adresseIpClient, sf::SocketTCP _socketTCPClient, Server* _pServer)/*:sf::Thread::Thread()*/
+{
+    m_NumeroClient = _numeroClient;
+    m_AdresseIpClient = _adresseIpClient;
+    m_SocketTCPClient = _socketTCPClient;
+    m_pServer = _pServer;
+}
+
+ThreadPrincipal::~ThreadPrincipal()
+//Destructeur
+{
+    delete m_pServer;
+}
+
+void ThreadPrincipal::Run()
+{
+    std::cout << "Thread principal du client n° : " << m_NumeroClient << " créé." << std::endl;
+}
