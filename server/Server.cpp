e07dac2c1bb38971b03613ca6b9a2d730e952faa
olivier.solliec <olivier.solliec@d04796a0-1e52-11df-b145-f921259a7feb>
2010-03-06 17:27:03 +0000

    Première esquisse du server, gestion uniquement du TCP dans le server.h et server.cpp ainsi que le threadPrincipal.h et threadPrincipal.cpp

--- server/Server.cpp
+++ server/Server.cpp
@@ -2,15 +2,19 @@
 #include "Server.h"
 #include "ThreadPrincipal.h"
 
-Server::Server(int _port)
+Server::Server(int const _port)
 {
     m_localAdress = sf::IPAddress::GetLocalAddress();
     m_portTCP = _port;
     m_nbClients = 0;
     m_socketTCP = sf::SocketTCP::SocketTCP();
     m_PartieEnCours = false;
+    Server::MInitialiserListeThreadPrincipaux ( m_ListeThreadPrincipaux );
 }
-
+Server::~Server()
+{
+    m_socketTCP.Close();
+}
 void Server::MAfficherStatus()
 {
     std::cout << "Serveur " << m_localAdress.ToString() << ":" << m_portTCP <<std::endl;
@@ -18,7 +22,6 @@ void Server::MAfficherStatus()
 void Server::MAttenteConnexion()
 {
     if (!m_socketTCP.Listen(m_portTCP))
-    //Si la socket n'arrive pas à écouter sur le port, gestion des erreurs
     {
         std::cout << "Erreur d'écoute de la socket TCP sur le port " << m_portTCP << std::endl;
     }
@@ -29,21 +32,37 @@ void Server::MAttenteConnexion()
         sf::IPAddress clientAddress;
         sf::SocketTCP socketClient;
         if (m_socketTCP.Accept(socketClient, &clientAddress) != sf::Socket::Done)
-        //en cas d'erreur d'une acceptation de socket
             {
                 std::cout<< "Erreur d'acceptation de client sur la socket du serveur" << std::endl;
             }
-        //si tout se passe bien
         else {
             std::cout<<"Connexion d'un nouveau client d'adresse : " << clientAddress.ToString() << std::endl;
             //création du thread principal
-            ThreadPrincipal tp(m_nbClients, clientAddress, socketClient, Server::MGetAdresse());
-            tp.Launch();
+            ThreadPrincipal* tp = new ThreadPrincipal(m_nbClients, clientAddress, socketClient, Server::MGetInstance());
+            tp->Launch();
             m_nbClients++;
             }
     }
 }
-Server::Server* Server::MGetAdresse()
+Server::Server* Server::MGetInstance()
 {
     return this;
+}
+bool Server::MGetStatusPartie()
+{
+    return m_PartieEnCours;
+}
+void Server::MAttenteFinPartie()
+{
+    while (m_PartieEnCours)
+    {
+    }
+}
+void Server::MInitialiserListeThreadPrincipaux(std::list<ThreadPrincipal*> _listeThreadPrincipaux)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        ThreadPrincipal *tp;
+        //_listeThreadPrincipaux.assign(*tp); << ça marche pas et j'ai pas envie de savoir pourquoi
+    }
 }
--- server/ThreadPrincipal.h
+++ server/ThreadPrincipal.h
@@ -2,25 +2,36 @@
 #define ThreadPrincipal_h
 #include <SFML/Network.hpp>
 #include "Server.h"
-
+#include <iostream>
 class ThreadPrincipal : public sf::Thread
-/* Thread qui s'occupe d'envoyer/recevoir des messages par TCP au client, et se charge de la création des sous threads pour prendre en charge l'envoi/reception des datagrammes UDP */
+/* Thread qui s'occupe d'envoyer des instructions par TCP au client, et se charge de la création des sous threads de communication */
 {
 
 public :
-    ThreadPrincipal(int _numeroClient, sf::IPAddress _adresseIpClient, sf::SocketTCP _socketTCPClient, Server::Server* _pServer);
+    /* Constructeur */
+    ThreadPrincipal(int const _numeroClient, sf::IPAddress const _adresseIpClient, sf::SocketTCP const _socketTCPClient, Server::Server const * _pServer);
+
+    /* Méthode run du thread */
     virtual void Run();
+
+    /* Destructeur */
     ~ThreadPrincipal();
 
+    /* Méthode qui envoie une instruction au client par TCP */
+    bool MEnvoiInstruction(std::string const _msg);
+
 private :
+    /* l'ordre d'arrivée du client sur le serveur */
     int m_NumeroClient;
+
+    /* Adresse ip du client */
     sf::IPAddress m_AdresseIpClient;
+
+    /* Socket TCP du client */
     sf::SocketTCP m_SocketTCPClient;
-    Server* m_pServer;
 
-
-
-
+    /* Pointeur vers le père, le serveur */
+    Server const * m_pServer;
 
 };
 
