07bd5c8a0fba2ec0a2040b731ed36b0facf021fa
olivier.solliec <olivier.solliec@d04796a0-1e52-11df-b145-f921259a7feb>
2010-03-07 13:12:41 +0000

    Version Volatile, reprendre ici en cas de problème avec la nouvelle version pointeur

--- trunk/server/ThreadEnvoi.cpp
+++ trunk/server/ThreadEnvoi.cpp
@@ -1,12 +1,33 @@
 #include "ThreadEnvoi.h"
+#include <iostream>
 
 ThreadEnvoi::ThreadEnvoi( uint const _portUDP, uint const _numClient, sf::IPAddress const _clientAddress)
 {
     m_portUDP = _portUDP;
     m_NumeroClient = _numClient;
     m_clientAddress = _clientAddress;
+    /* Par défaut, lorsqu'on construit un thread de communication, c'est parce qu'une partie a commencé */
+    m_PartieEnCours = true;
 }
 ThreadEnvoi::~ThreadEnvoi()
 {
 
+}
+void ThreadEnvoi::Run()
+{
+    std::cout << "THREAD ENVOI" << m_NumeroClient << std::endl;
+    while ( m_PartieEnCours )
+    {
+        std::cout << "haha" << std::endl;
+    }
+}
+bool ThreadEnvoi::MGameStop()
+{
+    m_PartieEnCours = false;
+    return true;
+}
+bool MEnvoiDonnees()
+{
+    std::cout << "haha" << std::endl;
+    return true;
 }
